f9aad3cc5cbe5c9d8d7a2fb125a32339948a979c
david gray <davidseangray@gmail.com>
2017-03-27 16:22:45 +0100

    changes made to remove useless data

--- directory.rb
+++ directory.rb
@@ -1,19 +1,4 @@
-#printing the list of students into an array
- [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-#all text placed into methods
-
+#A method that takes user input and places it as a hash into a new array
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"

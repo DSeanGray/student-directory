8c285d09e104d1184f6eda1ba83456e36452869e
david gray <davidseangray@gmail.com>
2017-03-27 16:20:06 +0100

    created a method that takes user input for student names

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
 #printing the list of students into an array
-students = [
+ [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
   {name: "Nurse Ratched", cohort: :november},
@@ -13,6 +13,26 @@ students = [
   {name: "Norman Bates", cohort: :november}
 ]
 #all text placed into methods
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create and empty array
+  students = []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student has to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "------------"
@@ -28,6 +48,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 #nothing happens in the program until these methods are called
+students = input_students
 print_header
 print(students)
 print_footer(students)

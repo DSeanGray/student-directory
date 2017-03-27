ff49852468dd55b0eb0d421c153eb629b9804e7e
david gray <davidseangray@gmail.com>
2017-03-27 15:22:45 +0100

    method added to puts students through iteration

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-student_count = 11
-#printing the list of students
+#printing the list of students into an array
+students = [
+ "Dr.Hanibal Lecter",
+ "Darth Vader",
+ "Nurse Ratched",
+ "Michael Coleone",
+ "Alex DeLarge",
+ "The Wicked Witch of the west",
+ "Terminator",
+ "Freddy Krueger",
+ "The Joker",
+ "Joffrey Baratheon",
+ "Norman Bates"
+]
+# now we print each index in the array
 puts "The students of Villains Academy"
 puts "------------"
-puts "Dr.Hanibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Coleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the west"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#total number of students being printed without a new line from puts
-puts "Overall, we have #{student_count} great students"
+students.each do |student|
+  puts student
+end
+#now we print to total
+puts "Overall, we have #{students.count} great students"

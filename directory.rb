0379eddb65741f951e63f5dd3930bd97e3637485
david gray <davidseangray@gmail.com>
2017-03-28 12:34:29 +0100

    Methods added to get user input and return the names that match the user input by first letter.

--- directory.rb
+++ directory.rb
@@ -18,23 +18,42 @@ def input_students
   students
 end
 
+
 def print_header
   puts "The students of Villains Academy"
-  puts "------------"
+  puts "-" * 12
 end
-#students now print out with a number starting from 1
+#getting input from the user for a first name first letter
+def user_input_letter
+  puts "Type the first letter of the student names you want to see and press return."
+  first_letter = gets.chomp
+end
+#a method that take the students array and asks it to print out array items if they meet
+#the IF coniditon that takes x from user_input_letter
+def students_by_first_letter(students, x)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with?(x)
+    puts "-" * 12
+  end
+end
+
+#students now print out with their index position starting at 1
 #each.with_index was used sto start at 1 as each_with_index takes no arguments and starts at 0
 def print(students)
   students.each.with_index(1) do |student, index|
     puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "-" * 12
   end
 end
+
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
+
 #nothing happens in the program until these methods are called
 students = input_students
 print_header
 print(students)
+students_by_first_letter(students, user_input_letter)
 print_footer(students)
